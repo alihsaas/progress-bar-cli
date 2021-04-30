3c1e16d792b2d5e33c5e72b4e61e230093653e57
alihsaas <alihsaas2@gmail.com>
2021-04-30 09:26:23 +0300

    add progress characters

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ struct ProgressBar {
     target: u64,
     current: u64,
     term: Term,
+    progress_chars: String,
     format: String,
 }
 
@@ -20,6 +21,7 @@ impl ProgressBar {
             current: 0,
             term: Term::stdout(),
             format: String::from("{progress}"),
+            progress_chars: String::from("#>-"),
         }
     }
 
@@ -36,9 +38,13 @@ impl ProgressBar {
             let progress_length = u64::try_from(w).unwrap();
             let progress_length: u64 = progress_length.wrapping_sub(self.format.replace("{progress}", "").len().try_into().unwrap());
 
-            let numeric_progress: u64 = (self.current * progress_length) / self.target;
-
-            let progress = String::from("#").repeat(numeric_progress.try_into().unwrap());
+            let numeric_progress: u64 = (self.current * (progress_length)) / self.target;
+            let progress_left = progress_length - numeric_progress;
+            let progress = format!("{}{}{}", 
+                self.progress_chars[0..1].to_string().repeat(usize::try_from(numeric_progress).unwrap() - 1usize),
+                self.progress_chars[1..2].to_string(),
+                self.progress_chars[2..3].to_string().repeat(progress_left.try_into().unwrap())
+            );
             let formated = self.format.replace("{progress}", progress.as_str());
             self.term.clear_line()?;
             self.term.write_str(&formated)?;
